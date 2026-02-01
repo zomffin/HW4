5ceed5a9f18c5b36d0722a108d99cb06b9e5818d
zom <matsunoisabel@gmail.com>
2026-02-01 13:17:19 -0800

    Add audio clips, work on game over

--- MG4/Assets/Scripts/UIHandler.cs
+++ MG4/Assets/Scripts/UIHandler.cs
@@ -7,15 +7,22 @@ public class UIHandler : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI _scoreText;
     [SerializeField] TextMeshProUGUI _highScoreText;
-    [SerializeField] GameObject _bangbang; 
+    [SerializeField] GameObject _bangbang;
+
+    [SerializeField] GameObject _gameOverScreen;
+    [SerializeField] TextMeshProUGUI _finalScore; 
+
+    
     
     private int _currScore;
     private int _highScore = 0;
+
+    private bool _gotHighScore = false; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GameController.Instance.Player.score += updateScore;
-        GameController.Instance.Player.end += resetScore; 
+        GameController.Instance.Player.end += gameOver; 
     }
 
     // Update is called once per frame
@@ -33,14 +40,26 @@ public class UIHandler : MonoBehaviour
         {
             _bangbang.SetActive(true); 
             _highScore = _currScore;
-            _highScoreText.text = "Highscore: " + _highScore; 
+            _highScoreText.text = "Highscore: " + _highScore;
+            _gotHighScore = true; 
+        }
+    }
+
+    void gameOver()
+    {
+        _gameOverScreen.SetActive(true);
+        _finalScore.text = "Score: " + _currScore + "\n High score: " + _highScore;
+        if (_gotHighScore)
+        {
+            _finalScore.text += "\n You got a new high score!"; 
         }
     }
 
     void resetScore()
     {
         _currScore = 0;
-        _bangbang.SetActive(false); 
+        _bangbang.SetActive(false);
+        _gameOverScreen.SetActive(false); 
     }
 
 }
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Write your Devlog here!
 - [Simple Bird Sprites](https://hredbird.itch.io/simple-bird-sprites) - Bird sprites
 - [Industrial Pipe Platformer Tileset](https://wwolf-w.itch.io/industrial-pipe-platformer-tileset) - Pipe and Ground sprites
 - [Double Exclamation Mark Emoji](https://emojiterra.com/double-exclamation-mark/) - Bang bang emoji, Google Noto Color Emoji 16.0
+- [Classic Game Action Positive 5](https://pixabay.com/sound-effects/film-special-effects-classic-game-action-positive-5-224402/) - Score sfx
+- [wah wah sad trombone](https://pixabay.com/sound-effects/film-special-effects-wah-wah-sad-trombone-6347/) - Game over sfx
